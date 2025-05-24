c05afe2647010f8316e2c5219c8ffa90ffa38278
cnantoninor <antonino.rau@gmail.com>
2025-05-23 20:09:44 -0400

    customized

--- chapter_2/connecting.py
+++ chapter_2/connecting.py
@@ -4,21 +4,24 @@ from dotenv import load_dotenv
 
 # Load API key from .env file
 load_dotenv()
-api_key = os.getenv('OPENAI_API_KEY')
+api_key = os.getenv("OPENAI_API_KEY")
 # Ensure the API key is available
 if not api_key:
     raise ValueError("No API key found. Please check your .env file.")
-client = OpenAI(api_key=api_key)
+client = OpenAI(base_url="http://localhost:1234/v1", api_key=api_key)
 
 
 # Example function to query ChatGPT
 def ask_chatgpt(user_message):
     response = client.chat.completions.create(
-        model="gpt-4-1106-preview",  # gpt-4 turbo or a model of your preference
-        messages=[{"role": "system", "content": "You are a helpful assistant."},
-                  {"role": "user", "content": user_message}],
+        model="devstral-small-2505",  # gpt-4 turbo or a model of your preference
+        # model="deepseek-r1-distill-qwen-7b",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_message},
+        ],
         temperature=0.7,
-        )       
+    )
     return response.choices[0].message.content
 
 
